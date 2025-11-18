9130aa50fc013f2cfcec0b834e12720a5a1415e5
Bakhtawar-tariq <28100508@lums.edu.pk>
2025-11-18 12:34:23 +0500

    implemented increase decrease postlikes, were easy as essentially are very similar...dont know if they are correct tho yet

--- Part1/src/SocialMediaSystem.cpp
+++ Part1/src/SocialMediaSystem.cpp
@@ -21,7 +21,13 @@ void SocialMediaSystem::buildUndiscoveredFeed(vector<int> likes) {
 }
 
 void SocialMediaSystem::updatePostLikes(int oldLikes, int newLikes) {
-    
+    int indx = findPostIndex(oldLikes);
+    if (indx == -1){
+        return;
+    }
+    else{
+        feed.increaseKey(oldLikes,newLikes);
+    }
 }
 
 void SocialMediaSystem::clearLowEngagementPosts(int threshold) {
@@ -34,5 +40,11 @@ vector<int> SocialMediaSystem::getTopNPosts(int N) {
 }
 
 void SocialMediaSystem::decreasePostLikes(int currentLikes, int newLikes) {
-    
+    int indx = findPostIndex(currentLikes);
+    if (indx == -1){
+        return;
+    }
+    else{
+        feed.decreaseKey(currentLikes,newLikes);
+    }
 }
