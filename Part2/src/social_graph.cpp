83e196c648d9c0d266e4e070af85751c4eef1f27
Bakhtawar-tariq <28100508@lums.edu.pk>
2025-11-27 22:08:09 +0500

    did hasedge and now since the functions needed for the first few tests are all complete and my tests arent passing, will work on fixing them

--- Part2/src/social_graph.cpp
+++ Part2/src/social_graph.cpp
@@ -86,6 +86,15 @@ void SocialGraph::removeEdge(int from, int to)
 bool SocialGraph::hasEdge(int from, int to) const
 {
     // TODO
+    auto it = adjList.find(from);
+    if (it == adjList.end()){
+        return false; //if either vertex doesnot exist return
+    }
+    for (int i = 0; i < it->second.size(); i++){
+        if (it->second[i] == to){
+            return true;
+        }
+    }
     return false;
 }
 
