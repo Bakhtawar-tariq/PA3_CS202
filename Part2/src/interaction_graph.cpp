12b982d383d4f9d48c469891d9cc08bf8e7df31e
Bakhtawar-tariq <28100508@lums.edu.pk>
2025-11-27 18:11:54 +0500

    implemented the remove function...took some while to navigate through the maps to understand but once done implementing was just simple and similar

--- Part2/src/interaction_graph.cpp
+++ Part2/src/interaction_graph.cpp
@@ -38,6 +38,34 @@ void InteractionGraph::removeVertex(int id, NodeType type)
     // TODO: Implement this function. This is the most complex one.
     // If you remove a USER, you must also clean up their interactions from the postToUserEdges map.
     // If you remove a POST, you must do the reverse from the userToPostEdges map
+    if (type == NodeType::USER){//remove some user
+        std::vector<Interaction>& user_posts = userToPostEdges.at(id); //getting all the posts user has interacted w
+        for (int i = 0; i < user_posts.size(); i++){
+            int postid = user_posts[i].targetID; //getting post id of each post user has interacted w
+            std::vector<Interaction>& post_int = postToUserEdges.at(postid); //getting all the users that interacted w this post (which our user also interacted w)
+            for (int j = post_int.size() - 1; j >= 0; j--){ //since erasing so going backwards wont affect indexes before it
+                if (post_int[j].targetID == id){ //removing user from that posts interactions
+                    post_int.erase(post_int.begin()+j);
+                }
+            }
+        }
+        userToPostEdges.erase(id);
+    }
+
+    else if( type == NodeType::POST){ //remove some post
+        //reversing logic of remove user
+        std::vector<Interaction>& post_int = postToUserEdges.at(id); //getting all the users that have interacted w the post
+        for (int i = 0; i < post_int.size(); i++){
+            int userid = post_int[i].targetID; //getting user id of each user that interacted w the post
+            std::vector<Interaction>& user_posts = userToPostEdges.at(userid); //getting all posts of that user which interacted w the post we wanna remove
+            for (int j = post_int.size() - 1; j >= 0; j--){
+                if (user_posts[j].targetID == id){ //removing the post from each users' interaction
+                    user_posts.erase(user_posts.begin()+j);
+                }
+            }
+        }
+        postToUserEdges.erase(id);
+    }
 }
 
 void InteractionGraph::addInteraction(int userID, int postID, int weight)
